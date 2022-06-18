ed6a9f7b0ae6764d2d1986b7029f5b439722b706
maniarisen <shiraki.milk.0403@gmail.com>
2022-06-18 20:06:09 +0900

    yeah

--- learn_simple.py
+++ learn_simple.py
@@ -15,29 +15,15 @@ import logging
 import sys
 from distutils.util import strtobool
 
-logger = logging.getLogger(__name__)
-fmt = "%(asctime)s %(levelname)s %(name)s :%(message)s"
-logging.basicConfig(level=logging.DEBUG, format=fmt)
-
-args = sys.argv
 
 #サーバとローカルどちらでも動作する形
 #テキストと最長出力長と最小出力長を入力して、モデルを保存し、保存したディレクトリを返す
-<<<<<<< HEAD
 def study_from_markov(text):
     n = 1
     text = markov.parse_text(text)
     #format = format
     max_chars = 75
     min_chars = 10
-=======
-def study_from_markov(text, max_chars, min_chars):
-    n = 1
-    text = markov.parse_text(text)
-    #format = format
-    max_chars = max_chars
-    min_chars = min_chars
->>>>>>> 2a7d08cdf5c87fb89e36596c033311509106bd87
     #logger.info('Parsed text.')
     #text_model = markov.build_model(text, format=format, state_size=2)
     text_model = markov.build_model(text, state_size=2)
@@ -56,44 +42,28 @@ def study_from_markov(text, max_chars, min_chars):
 #print('Usage: python learn.py <filename> [max_chars] [min_chars]')
 
 # .envファイルの内容を読み込見込む
-load_dotenv()
-max_chars = int(args[2]) if args[2:3] else 70
-min_chars = int(args[3]) if args[3:4] else 25
+if __name__ == '__main__':
+    logger = logging.getLogger(__name__)
+    fmt = "%(asctime)s %(levelname)s %(name)s :%(message)s"
+    logging.basicConfig(level=logging.DEBUG, format=fmt)
 
-if args[1] == '^@*.':
-    tweets = getTweet(Account)
-    
-try:
-    filename = args[1]
-    filepath = 'data/' + filename + '.txt'
-    text = open(filepath, 'r').read()
-    #format = bool(strtobool(args[2])) if args[2:3] else True
+    args = sys.argv
+    load_dotenv()
+    max_chars = int(args[2]) if args[2:3] else 70
+    min_chars = int(args[3]) if args[3:4] else 25
 
-    #parsed_text = generate_sentence(text, format, max_chars, min_chars)
-    print(study_from_markov(text, max_chars, min_chars))
-    #text = generate_from_markov(path)
-except IndexError:
-    print('ERROR: filename is required. (e.g. "sample")')
-    sys.exit()
-<<<<<<< HEAD
+    if args[1] == '^@*.':
+        tweets = getTweet(Account)
+        
+    try:
+        filename = args[1]
+        filepath = 'data/' + filename + '.txt'
+        text = open(filepath, 'r').read()
+        #format = bool(strtobool(args[2])) if args[2:3] else True
 
-
-"""
-1. Load text -> Parse text using MeCab
-"""
-#parsed_text = markov.parse_text('data/' + filename + '.txt')
-
-"""
-2. Build model
-"""
-
-# Load from JSON
-# json = open('input.json').read()
-# text_model = markovify.Text.from_json(json)
-
-
-"""
-3. Make sentences
-"""
-=======
->>>>>>> 2a7d08cdf5c87fb89e36596c033311509106bd87
+        #parsed_text = generate_sentence(text, format, max_chars, min_chars)
+        print(study_from_markov(text))
+        #text = generate_from_markov(path)
+    except IndexError:
+        print('ERROR: filename is required. (e.g. "sample")')
+        sys.exit()

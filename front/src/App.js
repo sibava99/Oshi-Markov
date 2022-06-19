e072c68033270c57284adb96d92dae397b092660
araichidu <yanonay3@gmail.com>
2022-06-19 09:22:34 +0900

    pic

--- front/src/App.js
+++ front/src/App.js
@@ -5,7 +5,9 @@ import Home from './Home.js'
 import FromTweet from './FromTweet.js'
 import FromCopus from './FromCopus.js'
 import Rules from './Rules.js'
+import Box from '@mui/material/Box';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Container from '@mui/material/Container';
 
 class App extends React.Component {
   constructor(props,context){
@@ -15,8 +17,10 @@ class App extends React.Component {
   render(){
     return (
       <BrowserRouter>
+      <Box sx={{ bgcolor: '#f0e9f6'}}>
         <div>
-            <Header/>
+        <Header/>
+        <Container>
             <Routes>
             <Route exaxt path="/" element = {<Home />} />
             <Route path="fromTweet" element = {<FromTweet />} />
@@ -24,7 +28,9 @@ class App extends React.Component {
             <Route path="rules" element = {<Rules />} />
             <Route element = {<NotFound />} />
             </Routes>
+            </Container>
         </div>
+        </Box>
         </BrowserRouter>
     );
   }
--- front/src/Home.js
+++ front/src/Home.js
@@ -7,10 +7,12 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Box from '@mui/material/Box';
+import ArticleIcon from '@material-ui/icons/Assignment';
 import { Link } from 'react-router-dom';
 import lome from "./pic/lome.jpg"
 import { Grid } from '@material-ui/core';
 import Container from '@mui/material/Container';
+import TwitterIcon from '@material-ui/icons/Twitter';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,8 +30,16 @@ export default function Home() {
     const classes = useStyles();
     return(
         <div>
+        <Box sx = {{height:100}}></Box>
+        <Box sx={{ bgcolor: '#ffffff', width: 900,  borderRadius: '16px'}}>
+        <Box sx = {{height:100}}></Box>
         <Salome />
+        </Box>
+        <Box sx = {{height:50}}></Box>
+        <Box sx={{ bgcolor: '#ffffff', width: 900,  borderRadius: '16px'}}>
         <Chalenge />
+        </Box>
+        <Box sx = {{height:50}}></Box>
         </div>
     );
 }
@@ -37,9 +47,10 @@ export default function Home() {
 function Salome(){
     return(
         <div align = "center">
-            <img src= {lome} alt="picture" width = "50%"/>
-            <br></br>
-            <Button color="inherit" >生成</Button>
+            <img src= {lome} alt="picture" width = "700"/>
+            <Box sx = {{height:50}}></Box>
+            <Button variant="contained" color="secondary" size = "large" >生成</Button>
+            <Box sx = {{height:50}}></Box>
         </div>
     );
 }
@@ -47,16 +58,20 @@ function Salome(){
 function Chalenge(){
     return(
         <div>
-            
-            <Container maxWidth="sm" color="secondary">
-            <h2>やってみよう！</h2>
-            <Grid container spacing={2}>
-              <Grid item xs = {6}><Button variant="contained" color="secondary" size = "large" component={Link} to = "/fromTweet">Tweetから生成</Button></Grid>
-              <Grid item xs = {6}><Button variant="contained" color="inherit" size = "large" component={Link} to = "/fromCopus">Copusから生成</Button></Grid>
+            <Container maxWidth="sm" >
               
-            </Grid>
+            <Box sx = {{height:50}}></Box>
+                <h2>やってみよう！</h2>
+                <Box sx = {{height:50 , borderTop:2, borderColor:"#f20d99"}}></Box>
+                <p>TwiterIDまたはテキストデータからマルコフ連鎖で推し語録を生成しよう！</p>
+                <Box sx = {{height:50 }}></Box>
+                <Grid container spacing={2}>
+                  <Grid item xs = {6}><Button variant="contained" color="secondary" size = "large" component={Link} to = "/fromTweet" startIcon={<TwitterIcon/>}>Tweetから生成</Button></Grid>
+                  <Grid item xs = {6}><Button variant="contained" color="inherit" size = "large" component={Link} to = "/fromCopus" startIcon={<ArticleIcon />}>Copusから生成</Button></Grid>
+                </Grid>
+                
+                <Box sx = {{height:50}}></Box>
             </Container>
-            
         </div>
     );
 }
